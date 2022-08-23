1be46226ad98353752a08488dac20f8304910c75
pika2uuu <foo@bar.com>
2022-08-23 12:06:59 +0900

    add 書き込みボタンの追加

--- insert_menu.js
+++ insert_menu.js
@@ -1,27 +1,40 @@
 
 let menu_bar = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-right-controls")
+
+// 切り抜きボタン
 let now_button = document.createElement('div');
 let time_placeholder = ''
-now_button.className = "clip-btn"
-
+now_button.className = "custom-btn"
 // ボタンの中のハサミの画像
 let now_button_icon = document.createElement('img')
 const clip_image_url = chrome.runtime.getURL('images/scissors.png')
 now_button_icon.src = clip_image_url
 now_button_icon.className = 'now-button-icon'
 now_button.appendChild(now_button_icon)
-
 // 切り抜きボタンのツールチップ
 let now_button_tooltip = document.createElement('span')
 now_button_tooltip.textContent = 'タイムスタンプをコメント欄へ記録'
 now_button_tooltip.className = 'tooltip'
 now_button.appendChild(now_button_tooltip)
-
 // 切り抜きボタンを押した時に出るポップアップ
 let now_button_popup = document.createElement('span')
 now_button_popup.textContent = '記録しました'
 now_button_popup.className = 'clicked-popup'
 now_button.appendChild(now_button_popup)
+// メニューバーに挿入
+menu_bar.prepend(now_button);
+
+// 書き込みボタン
+let write_button = document.createElement('div')
+write_button.className = 'custom-btn'
+//書き込みボタンの画像
+let write_button_icon = document.createElement('img')
+const write_image_url = chrome.runtime.getURL('images/write.png')
+write_button_icon.src = write_image_url
+write_button_icon.className = 'write-button-icon'
+write_button.appendChild(write_button_icon)
+// メニューバーに挿入
+menu_bar.prepend(write_button);
 
 
 // 現在の再現方法
@@ -60,5 +73,3 @@ now_button.addEventListener('click', function() {
 //     });
 //     observer.observe(targetNode, config)
 // }, 4000);
-
-menu_bar.prepend(now_button);
