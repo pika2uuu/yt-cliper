2eafa3e2770ccb3bbe18c647dc34a2eccd19e1ff
pika2uuu <foo@bar.com>
2022-08-23 11:18:57 +0900

    clean リファクタリング

--- insert_menu.js
+++ insert_menu.js
@@ -9,16 +9,19 @@ let now_button_icon = document.createElement('img')
 const clip_image_url = chrome.runtime.getURL('images/scissors.png')
 now_button_icon.src = clip_image_url
 now_button_icon.className = 'now-button-icon'
+now_button.appendChild(now_button_icon)
 
 // 切り抜きボタンのツールチップ
 let now_button_tooltip = document.createElement('span')
 now_button_tooltip.textContent = 'タイムスタンプをコメント欄へ記録'
 now_button_tooltip.className = 'tooltip'
+now_button.appendChild(now_button_tooltip)
 
 // 切り抜きボタンを押した時に出るポップアップ
 let now_button_popup = document.createElement('span')
 now_button_popup.textContent = '記録しました'
 now_button_popup.className = 'clicked-popup'
+now_button.appendChild(now_button_popup)
 
 
 // 現在の再現方法
@@ -28,6 +31,7 @@ now_button_popup.className = 'clicked-popup'
 
 // コメント欄が現れないときのボタンのイベント
 now_button.addEventListener('click', function() {
+    // 再生時間をlocalStrangeに追記する
     let play_time = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span:nth-child(2) > span.ytp-time-current").innerText;
     play_time = `${play_time}  \n`
     time_placeholder = `${time_placeholder}${play_time}`
@@ -41,25 +45,20 @@ now_button.addEventListener('click', function() {
 // placeholder をクリックしてコメント欄を出現させる
 // document.querySelector("#placeholder-area").click()
 
-コメント欄が出現するのを監視
-window.setTimeout(() =>{
-    const targetNode = document.querySelectorAll("#contents")[3]
-    if(targetNode == null){
-        alert('存在しません')
-    } else {
-        alert('存在する')
-    }
-
-    const config = { childList: true };
-    const observer = new MutationObserver(() => {
-        alert('コメント欄が出現しました。');
-    });
-    observer.observe(targetNode, config)
+// コメント欄が出現するのを監視
+// window.setTimeout(() =>{
+//     const targetNode = document.querySelectorAll("#contents")[3]
+//     if(targetNode == null){
+//         alert('存在しません')
+//     } else {
+//         alert('存在する')
+//     }
+//
+//     const config = { childList: true };
+//     const observer = new MutationObserver(() => {
+//         alert('コメント欄が出現しました。');
+//     });
+//     observer.observe(targetNode, config)
 }, 4000);
 
-now_button.appendChild(now_button_icon)
-now_button.appendChild(now_button_tooltip)
-now_button.appendChild(now_button_popup)
 menu_bar.prepend(now_button);
-
-
