767d87645bffb8cb0fe538b50f17bef873a1a2b7
pika2uuu <foo@bar.com>
2022-08-23 21:24:53 +0900

    fix ページ移動で変数が更新されないバグを直した
    クリックでページ移動してもグローバル変数は更新されない。そこでイベントごとに変数を宣言した。

--- insert_menu.js
+++ insert_menu.js
@@ -42,10 +42,6 @@ write_button.appendChild(write_button_tooltip)
 // メニューバーに挿入
 menu_bar.prepend(write_button);
 
-const url = new URL(window.location)
-const video_id = url.searchParams.get("v")
-const local_storage_data = window.localStorage.getItem(video_id)
-let time_placeholder = ( local_storage_data == null) ? '' : local_storage_data
 
 // 現在の再現方法
 // コメント欄が現れるまでスクロールする
@@ -54,9 +50,13 @@ let time_placeholder = ( local_storage_data == null) ? '' : local_storage_data
 
 // コメント欄が現れないときのボタンのイベント
 now_button.addEventListener('click', function() {
+    // ページのデータ(グローバルスコープに置くとページ移動しても下の4つのデータが更新されない)
+    const url = new URL(window.location)
+    const video_id = url.searchParams.get("v")
+    const local_storage_data = window.localStorage.getItem(video_id)
+    let time_placeholder = ( local_storage_data == null) ? '' : local_storage_data
     // 再生時間をlocalStrangeに追記する
     let play_time = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span:nth-child(2) > span.ytp-time-current").innerText;
-    play_time = `${play_time}　\n`
     play_time = `${play_time}　\n` // 全角1文字空ける
     time_placeholder = `${time_placeholder}${play_time}`
     window.localStorage.setItem(video_id, time_placeholder)
@@ -67,6 +67,11 @@ now_button.addEventListener('click', function() {
 // 配信中はイベントリスナーつけない
 if (is_not_live) {
     write_button.addEventListener('click', () => {
+        // ページのデータ(グローバルスコープに置くとページ移動しても下の4つのデータが更新されない)
+        const url = new URL(window.location)
+        const video_id = url.searchParams.get("v")
+        const local_storage_data = window.localStorage.getItem(video_id)
+        let time_placeholder = ( local_storage_data == null) ? '' : local_storage_data
         // 画面の底までスクロールさせるとコメント欄が出現するが、画面の横幅によって底の要素が違うのでbreakpointで識別
         const breakpoint = 1016
         const window_width = window.outerWidth
@@ -82,6 +87,9 @@ if (is_not_live) {
             const comment = time_placeholder
             const text_area = document.querySelector("yt-formatted-string > #contenteditable-root")
             text_area.insertAdjacentText('beforeend', comment)
+            // データを共有している変数を空にする
+            time_placeholder = ''
+            // localStorage から保存したタイムスタンプを削除
             window.localStorage.removeItem(video_id)
         }, 2000)
     })
